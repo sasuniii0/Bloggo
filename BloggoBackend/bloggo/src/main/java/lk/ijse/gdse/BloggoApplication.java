f56e1f33362d4088a7bd5d950c1d217ea3b113ac
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-17 18:44:27 +0530

    initialize project structure with Spring Boot and Maven configuration

---
+++ BloggoBackend/bloggo/src/main/java/lk/ijse/gdse/BloggoApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggoApplication.class, args);
+	}
+
+}
